bbc332dd35c0f6a74b791742d3db7bc6db06906a
fatbone008 <165080496@qq.com>
2018-04-06 17:21:14 +0800

    add wechat middleware

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var wechat = require('./wechat');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log('the res we get:', res);
-  // res.render('index', { title: 'Express' });
-});
+router.use('/wechat', wechat);
 
 module.exports = router;
