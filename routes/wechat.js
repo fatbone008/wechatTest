9c1d2184f1fd134172ee5b0c42cd59e875e43023
fatbone008 <165080496@qq.com>
2018-04-07 16:38:29 +0800

    sha1 and return

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto')
 
 
 router.get('/', function (req, res) {
-    console.log("-------req.query:", req.query);
-    res.send('Birds home page')
+    var q = req['query'];
+    var a = [];
+    a.push('fawefawef');
+    a.push(q['timestamp']);
+    a.push(q['nonce']);
+
+    var sha1 = crypto.createHash("sha1");
+    sha1.update(a.sort().join(''));
+    var hack = sha1.digest("hex");
+
+    if(q['signature'] === hack) {
+        console.log("hack: ", hack );
+        res.send(q['echostr']);
+    }else{
+        console.log("hack: ", nothing);
+        res.send('Birds home page');
+    }
 })
 
 module.exports = router;
