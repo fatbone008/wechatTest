1d56d0722ea3d30f398834bd259302cd7a8f7b20
fatbone008 <165080496@qq.com>
2018-06-17 18:22:41 +0800

    微信认证

--- Server.js
+++ Server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const dataFetch = require('./routes/dataFetch')
-const fs = require('fs')
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -15,4 +14,4 @@ app.get('*', function(req, res) {
     console.log("come on");
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 80);
