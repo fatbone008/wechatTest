2c17c6245d0bd8f038d3fc68f1f8f3a66492f00a
fatbone008 <165080496@qq.com>
2018-06-18 16:51:12 +0800

    转发试验

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.use('/wechat', wechat);
 
 router.get('/firstPage', (req, res) => {
     console.log("转发授权页面req: ")
-    res.redirect("http://anniesreading.com/firstPage");
+    res.redirect("http://anniesreading.com:3000/firstPage");
 })
 
 module.exports = router;
