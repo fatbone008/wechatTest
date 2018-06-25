2b12b974b76206ce92a818fc25e2489c84d941bb
陈贻辉 <yihui.chen@esmart365.com>
2018-06-25 15:03:58 +0800

    save user

--- routes/dataFetch.js
+++ routes/dataFetch.js
@@ -9,6 +9,10 @@ router.get('/testingJson', (req, res, next) => {
     res.json({'ab': 'cd'});
 })
 
+/**
+ * 获取到code后请求换区openid，拿到openid后保存到数据库成功了才返回openid给前端。
+ * 保存openid通过Seven写的脚本服务来完成：localhost：2000/user/*** 接口
+ */
 router.get('/getOpenId', (req, res, next) => {
     const code = req.query.code;
 
@@ -17,15 +21,20 @@ router.get('/getOpenId', (req, res, next) => {
     const appSecret = 'f0ec7e371f1c6afa0dfb34f6cdb196b3'
 
     console.log("即将向微信发送code换区openID -------------》》》");
+    // get openid through code ----------------->> code
     request(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=${appSecret}&code=${code}&grant_type=authorization_code`
         , function (error, response, body) {
 
         if (!error && response.statusCode == 200) {
             console.log(body)
             if(body['openid']){
-                // save the openid/userid to mysql
+                // save the openid/userid to mysql ---------------->> openid
                 sendQuizingServer(body['openid'], res => {
                     res.json(body['openid'])
+                }, error => {
+                    console.error("openId保存失败")
+                    res.write('openId保存失败');
+                    res.sendStatus(500);
                 })
             } else {
                 console.error("微信没有返回openid:", body);
