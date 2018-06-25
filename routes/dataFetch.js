8775876989f804ce76b528c776760466a6473ea1
fatbone008 <165080496@qq.com>
2018-06-25 23:38:04 +0800

    change user

--- routes/dataFetch.js
+++ routes/dataFetch.js
@@ -53,7 +53,7 @@ router.get('/getOpenId', (req, res, next) => {
 
 var sendQuizingServer = function (userid) {
     return new Promise((resolve, reject) => {
-        request('http://47.105.49.208:2000/use/' + userid, function (error, response, body) {
+        request('http://47.105.49.208:2000/user/' + userid, function (error, response, body) {
             if(!error && response.statusCode === 200){
                 console.log("保存userid成功:", body);
                 resolve(body);
