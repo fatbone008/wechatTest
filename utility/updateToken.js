c76408e44f873b79fc09e916421a5a1c41365414
陈贻辉 <yihui.chen@esmart365.com>
2018-07-04 17:08:09 +0800

    chapter to qa

--- utility/updateToken.js
+++ utility/updateToken.js
@@ -4,7 +4,7 @@ var https = require('https');
 
 var qingqiu = function() {
     return new Promise((resolve,reject) => {
-        https.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa43458bc62176324&secret=9ff7423489c2d33d535eb0d0f81c59fc`, (res) => {
+        https.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx45c5430dac975c29&secret=f0ec7e371f1c6afa0dfb34f6cdb196b3`, (res) => {
             console.log('状态码：', res.statusCode);
             console.log('请求头：', res.headers);
 
