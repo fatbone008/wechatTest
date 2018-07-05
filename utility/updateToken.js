9faa9d0912379d9a19661b7faeb8d64538ffd62a
fatbone008 <165080496@qq.com>
2018-07-06 00:57:41 +0800

    improve access_token

--- utility/updateToken.js
+++ utility/updateToken.js
@@ -2,6 +2,11 @@ var config = require('../config/wechat.config');
 var access_token;
 var https = require('https');
 
+/**
+ * 这个请求向微信发送请求，获取access_token后，继续向微信发送第二次请求，获取到jsapi_ticket。
+ * 随后将jsapi_ticket放入access_token的jsapi_ticket属性里一起返回。
+ * @returns {Promise<any>}
+ */
 var qingqiu = function() {
     return new Promise((resolve,reject) => {
         https.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx45c5430dac975c29&secret=f0ec7e371f1c6afa0dfb34f6cdb196b3`, (res) => {
@@ -16,7 +21,7 @@ var qingqiu = function() {
                         console.log(`https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${d['access_token']}&type=jsapi`);
                         let jsapi_ticket = JSON.parse(data.toString());
                         console.log('jsapi_ticket:', jsapi_ticket);
-                        access_token['jsapi_ticket'] = jsapi_ticket;
+                        access_token['jsapi_ticket'] = jsapi_ticket['ticket'];
                         // process.stdout.write(access_token);
                         resolve(access_token);
                     })
