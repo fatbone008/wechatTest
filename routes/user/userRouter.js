46ae8105a17293038d4e11dfbf027c57fa7361e3
fatbone008 <165080496@qq.com>
2018-07-08 21:30:18 +0800

    get access

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/getAccessToken_JsapiTicket', function (req, res, next) {
     console.log('向80端口转发请求access_token');
-    res.redirect('http://localhost:80/getToken');
+    res.redirect('http://anniesreading.com:80/getToken');
 });
 
 module.exports = router
