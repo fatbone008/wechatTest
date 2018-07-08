e6b6556ee83b434ebd0d5a2e0487b2891776fb49
fatbone008 <165080496@qq.com>
2018-07-08 22:37:09 +0800

    get access

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 router.get('/getAccessToken_JsapiTicket', function (req, res, next) {
     console.log('向80端口转发请求access_token');
-    res.redirect('http://anniesreading.com:80/getToken');
+    // res.redirect('http://anniesreading.com:80/getToken');
+    request.get(`http://anniesreading.com:80/getToken`, function (error, response, body) {
+        const token = JSON.parse(body);
+        if (!error && response.statusCode == 200 && token) {
+            res.json(token);
+        } else {
+            res.write('获取不到access_token');
+            res.sendStatus(500);
+        }
+    });
 });
 
 module.exports = router
